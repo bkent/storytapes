b5ed1f42cf9642b9926d928be84f8d6b285bba35
Ben Kent <mr.bkent@gmail.com>
2014-08-02 00:27:52 +0100

    Cover artwork images added
    Images stored and read from binary blobs in the database

--- functions.php
+++ functions.php
@@ -17,7 +17,24 @@ function iConnect2()
 }
 
 function ukstrtotime($str) {
-return strtotime(preg_replace("/^([0-9]{1,2})[\/\. -]+([0-9]{1,2})[\/\. -]+([0-9]{1,4})/", "\\2/\\1/\\3", $str));
+	return strtotime(preg_replace("/^([0-9]{1,2})[\/\. -]+([0-9]{1,2})[\/\. -]+([0-9]{1,4})/", "\\2/\\1/\\3", $str));
+}
+
+function DisplayImage($id) {
+
+    $query = "SELECT file,type FROM artwork WHERE titleid = ". $id;
+
+	$mysqli = iConnect2();
+	$data = $mysqli->query($query); 
+	$num_rows = $data->num_rows;
+	if ($num_rows > 0)
+	{
+		$row = $data->fetch_array();
+		$mime = $row["type"];
+
+		$b64Src = "data:".$mime.";base64," . base64_encode($row["file"]);
+		return '<img src="'.$b64Src.'" alt="" width="60px"/>';
+	}
 }
 
 ?>
